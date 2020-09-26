18e0d0d66318ebd78298a9f3ce4fca3cee03357e
DekelMalul <dekelmalul@gmail.com>
2020-09-26 20:55:11 +0300

    finished with annotation

--- git_info.py
+++ git_info.py
@@ -16,36 +16,50 @@ def git_user_validation(user,password):
         print("User or password isn`t valid", user)
         exit()
 
+'''
+This function check if the given organization name is valid
+'''
 def org_check(org,user,password):
-    url = "https://api.github.com/orgs" + org
+    url = "https://api.github.com/orgs/" + org
     respone = requests.request("Get", url, auth=(user,password))
     if respone.ok:
         print("We found", org , "organization")
     else:
         print("It seems that your organization dosen't exist: ", org)
         exit()
-        
+'''
+This function gather all the repos, counts the bracnch on each repo and 
+prints table.
+'''        
 def org_repos_info(org,user,password):
+    table_data = list()
     url = "https://api.github.com/orgs/" + org + "/repos" 
     respone = requests.request("Get", url, auth=(user,password))
     repo_list = json.loads(respone.text)
-    table_data = list()
+    
+    # running on each repo from the repos found in the organization
     for repo in repo_list:
         reponame = repo.get("name")
         branch_url = ("https://api.github.com/repos/" + org + "/" +
         reponame +"/branches")  
         respone = requests.request("Get", branch_url, auth=(user,password))
+        
+        # Checking if the given user finished it's api calls
         if not respone.ok:
             if respone.text.find("higher rate limit") != -1:
                 print ("no more api calls for ", user)
                 table = tabulate(table_data, headers = ["Repository","brauch count"])
                 print(table)
                 exit()
+        
+        # Checking if the repo is empty.
         if len (respone.text) == 2:
             table_data.append([reponame,0])
         else:
             branch_info = json.loads(respone.text)
             table_data.append([reponame,len(branch_info)])
+    
+    # Creating finale table and priting it out.
     table = tabulate(table_data, headers = ["Repository","brauch count"])
     print(table)
     
@@ -56,7 +70,7 @@ def main():
         opts, args = getopt.getopt(argv,"u:e:o:",["username =","email =","org ="])   
      except: 
         print("Enter valid parameters") 
-     print(opts)  
+      
      # Setting command line parameters
      for opt, arg in opts:
         if opt in ['-u', '--username']: 
